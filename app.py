fcb92dd849b789ce7df11a9988243a9190da6b21
Balson Gorai <balsongorai@gmail.com>
2023-08-20 18:00:57 +0530

    1st commit query params

--- app.py
+++ app.py
@@ -14,4 +14,21 @@ def index():
     4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
     5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
     """
-    return jsonify("TODO")
+    
+    fname = request.args.get("fname")
+    lname = request.args.get("lname")
+    if not fname and not lname:
+        return jsonify({"data":"Please provide either fname or lname"})
+    elif not fname:
+        response = {"data":f"Hello Mr {lname} !"}
+    elif not lname:
+        response = {"data":f"Hello, {fname} !"}
+    else:
+        response = {"data":f"Is your name {fname} {lname}."}
+        
+    return jsonify(response)
+
+@app.route("/")
+def hello():
+    response = {"data":"Hello World !!!"}
+    return jsonify(response)
